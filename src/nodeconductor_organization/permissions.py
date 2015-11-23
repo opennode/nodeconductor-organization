b5da5a6389d4275b47f5c3a7f8e3d266c5d84d73
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-11-23 18:03:06 +0200

    Remove redundant permission class
    -SAAS-348

--- src/nodeconductor_organization/permissions.py
+++ src/nodeconductor_organization/permissions.py
@@ -2,23 +2,6 @@ from __future__ import unicode_literals
 
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-from nodeconductor.structure.models import CustomerRole
-
-
-class IsAdminOrReadOnly(BasePermission):
-    """
-    Allows full access to admin users.
-    For other users read-only access.
-    """
-
-    def has_permission(self, request, view):
-        user = request.user
-
-        if user.is_staff or request.method in SAFE_METHODS:
-            return True
-
-        return False
-
 
 class OrganizationUserPermissions(BasePermission):
     """
