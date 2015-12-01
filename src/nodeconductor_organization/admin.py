179c7acc55ce01db753f768a6077583df03f7c4c
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-12-01 23:56:05 +0200

    Fix organization user customer field
    NC-971

--- src/nodeconductor_organization/admin.py
+++ src/nodeconductor_organization/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 from nodeconductor_organization.models import Organization, OrganizationUser
 
 
-admin.site.register(Organization)
-admin.site.register(OrganizationUser)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('abbreviation', 'native_name', 'customer', 'uuid')
+    list_filter = ('abbreviation', 'native_name')
+    ordering = ('abbreviation', 'native_name', 'customer')
+
+
+class OrganizationUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_approved', 'organization')
+    list_filter = ('is_approved', 'user')
+    ordering = ('user', 'is_approved', 'organization')
+
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(OrganizationUser, OrganizationUserAdmin)
